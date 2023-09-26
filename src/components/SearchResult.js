f9a9b4cb924cec15e22e773acabebbf617f9bcfd
strongerDeer <stronger_deer@naver.com>
2023-09-26 22:40:29 +0900

    📝 Edit: 검색결과 노출 컨텐츠 수정

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -4,10 +4,11 @@ export default class SearchResult {
   $searchResult = null;
   data = null;
 
-  constructor({ $target, initialData, onClick }) {
+  constructor({ $target, initialData, onClick, keyword }) {
     this.$target = $target;
     this.data = initialData;
     this.onClick = onClick;
+    this.keyword = keyword;
   }
 
   setState(nextData) {
@@ -24,11 +25,12 @@ export default class SearchResult {
     const $wrapper = createHTMLElement('section', { class: 'search-result' });
 
     const $ul = createHTMLElement('ul');
-    const $searchTitle = createHTMLElement(
-      'h2',
-      null,
-      dataLength !== 0 ? `검색결과 ${dataLength}개` : `검색결과가 없습니다.`,
-    );
+    const $searchTitle = createHTMLElement('h2', null);
+    $searchTitle.innerHTML =
+      dataLength !== 0
+        ? `<p>검색결과 <strong>${dataLength}</strong></p>`
+        : '검색결과가 없습니다.';
+
     $wrapper.appendChild($searchTitle);
     $wrapper.appendChild($ul);
     this.$target.appendChild($wrapper);
@@ -43,9 +45,28 @@ export default class SearchResult {
         src: book.thumbnail,
         alt: '',
       });
-      const $bookTitle = createHTMLElement('p', null, book.title);
-      const $bookAuthors = createHTMLElement('p', null, book.authors);
-      const $bookPublisher = createHTMLElement('p', null, book.publisher);
+      const $bookTitle = createHTMLElement(
+        'p',
+        { class: 'book-title', title: book.title },
+        book.title,
+      );
+
+      const $bookContent = createHTMLElement(
+        'p',
+        { class: 'book-contents' },
+        book.contents.slice(0, 80),
+      );
+      const $bookAuthors = createHTMLElement(
+        'p',
+        { class: 'book-authors', title: book.authors },
+
+        `저자: ${book.authors}`,
+      );
+      const $bookPublisher = createHTMLElement(
+        'p',
+        { class: 'book-publisher', title: book.publisher },
+        `출판사: ${book.publisher}`,
+      );
 
       $button.addEventListener('click', (e) => {
         const index = Number(e.currentTarget.getAttribute('data-index'));
@@ -54,6 +75,7 @@ export default class SearchResult {
 
       $button.appendChild($thumbnail);
       $button.appendChild($bookTitle);
+      $button.appendChild($bookContent);
       $button.appendChild($bookAuthors);
       $button.appendChild($bookPublisher);
       $li.appendChild($button);
